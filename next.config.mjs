1305b56d60f62488783fb02af343e6eaec8f8cc7
Redouan Hamdi <finance@service-pro.nl>
2026-05-15 00:24:29 +0200

    Remove custom distDir

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: process.env.NODE_ENV === "production" ? ".next-servicepro-build" : ".next-servicepro"
+
 };
 
 export default nextConfig;
