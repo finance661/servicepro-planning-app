148313b73975d7aa9c989e8b4710d3aba1dc9e18
Redouan Hamdi <finance@service-pro.nl>
2026-05-15 10:21:50 +0200

    Fix: simplify middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,1 @@
-export { default } from 'next-auth/middleware';
-
-export const config = {
-  matcher: [
-    '/planning/:path*',
-    '/instellingen/:path*',
-    '/api/sheets/:path*',
-    '/api/export/:path*',
-    '/api/settings/:path*',
-  ],
-};
+export {};
